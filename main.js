afb84d10a4dbe770eb34079a763193218e8a773d
Adam Vaughn <adammathewvaughn@gmail.com>
2021-08-31 21:11:03 -0500

    added background color and text

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Vaughn Engineering by Vite</h1>
+  <a href="https://www.linkedin.com/in/vaughn-software-engineering/" target="_blank">Contact Us Today!</a>
 `;
