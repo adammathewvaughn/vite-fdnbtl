1d42fb261661e77734211807ea5062bb05f4b311
adammathewvaughn <adammathewvaughn@gmail.com>
2022-07-15 23:16:05 -0500

    vanilla JS project

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>Vaughn Engineering by Vite</h1>
-  <a href="https://www.linkedin.com/in/vaughn-software-engineering/" target="_blank">Contact Us Today!</a>
+  <h1>AMV Embedded Systems</h1>
+  <a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank">Contact Us Today!</a>
 `;
