585aa86ae826da6ac90aa4b5d93939b943dd695d
adammathewvaughn <adammathewvaughn@gmail.com>
2022-08-06 21:01:30 -0500

    2 files modified

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <h1>AMV Embedded Systems</h1>
-  <h4>"Automating a better tomorrow"</h4>
-  <div>
-    <ul>
-      <li>Home</li>
-      <li>Auto</li>
-    </ul>
-  </div>
-  <a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank">Contact Us Today!</a>
+<div>
+<h1>AMV Embedded Systems</h1>
+<h4>"Automating a better tomorrow"</h4>
+<ul>
+<li>Home</li>   
+<li>Auto</li>
+</ul>
 
-`;
+</div>
+<div>
+   <a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank">Contact Us Today!</a>
+   </div>
+   
+   `;
