a9b25ae1af340703c0ab40f226472c6c0b2f5b8a
adammathewvaughn <adammathewvaughn@gmail.com>
2022-08-14 20:16:47 -0500

    2 files modified

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
+
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-<div class="main">
-  <h1>AMV Embedded Systems</h1>
-  <h4>"Automating a better tomorrow"</h4>
-  <ul>
-    <li>Home</li>   
-    <li>Auto</li>
-  </ul>
-  <div>
-    <div class="post">
-      <h4>Post a Comment</h4>
-      <input type="text">
+let content =  document.addEventListener("click", function getInputValue() {
+  document.getElementById("textInput").value;
+  
+    })
+    document.querySelector('#app').innerHTML = `
+
+<div class="header">
+<h1>AMV Embedded Systems</h1>
+<h4>"Automating a better tomorrow"</h4>
+</div>
+
+<div class="main">  
+<div class="post">
+      <h4onclick="getInputValue();">Post a Comment</h4>
+      <input id= "textInput" type="text" placeholder="Comment" >
     </div>
     <div class="comments">
-      <h3>Comments</h3>
-      <span></span>
+    <h3>Comments</h3>
+    <span>${content}</span>    
     </div>
+</div>
+<ul>
+<li>Home</li>   
+  <li>Auto</li>
+  </ul>
+  <div>
+  <a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank">Contact Us Today!</a>
   </div>
-    <div>
-    <a href="https://www.linkedin.com/in/adam-m-vaughn/" target="_blank">Contact Us Today!</a>
-    </div>
- </div>
-   `;
+  `;
+  
